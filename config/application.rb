9ea4fdd0ed6f6edc42523309aee38266efc598dc
mattjang96 <mattjang95@gmail.com>
2019-05-06 17:04:55 -0700

    stufff

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,5 @@
 require_relative 'boot'
-
+require "active_storage/engine"
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -10,7 +10,7 @@ module Shuttr
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
