9368b685b0e2fe5435921469aac50d3d1e0363ff
mattjang96 <mattjang95@gmail.com>
2019-06-06 07:03:18 -0700

    import albums reducer to entities and errors reducers

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import usersReducer from './users_reducer';
 import photosReducer from './photos_reducer';
 import commentsReducer from './comments_reducer';
+import albumsReducer from './albums_reducer';
 
 const entitiesReducer = combineReducers({
     users: usersReducer,
     photos: photosReducer,
-    comments: commentsReducer
+    comments: commentsReducer,
+    albums: albumsReducer,
 });
 
 export default entitiesReducer;
--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import sessionErrorsReducer from './session_errors_reducer';
 import photosErrorsReducer from './photos_errors_reducer';
 import commentsErrorsReducer from './comments_errors_reducer';
+import albumsErrorsReducer from './albums_errors_reducer';
 
 const errorsReducer = combineReducers({
     session: sessionErrorsReducer,
     photos: photosErrorsReducer,
     comments: commentsErrorsReducer,
+    albums: albumsErrorsReducer
 });
 
 export default errorsReducer;
